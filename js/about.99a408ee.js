a6bf0ec4be824597ceaf49d300868ee5f03a1f6e
Wei Mo <miloweimo@outlook.com>
2023-08-24 13:49:24 +0800

    auto build

---
+++ js/about.99a408ee.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkhahawode"]=self["webpackChunkhahawode"]||[]).push([[443],{9030:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var a=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},u=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"about"},[n("h1",[e._v("This is an about page")])])}],s=n(1001),l={},r=(0,s.Z)(l,a,u,!1,null,null,null),c=r.exports}}]);
+//# sourceMappingURL=about.99a408ee.js.map
